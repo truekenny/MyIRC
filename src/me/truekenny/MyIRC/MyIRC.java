b3a4d51d093855b02f265aaf674df9d1e64fff8d
Truekenny <myirc@tace.ru>
2014-07-28 19:32:24 +0400

    Вывод сообщений в консоль: вход, выход пользователя, сообщения пользователя.

---
+++ src/me/truekenny/MyIRC/MyIRC.java
@@ -0,0 +1,73 @@
+package me.truekenny.MyIRC;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.logging.Logger;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.World;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.PluginManager;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import com.google.common.collect.Lists;
+
+/**
+ * Главный класс плагина
+ * @author truekenny
+ */
+public class MyIRC extends JavaPlugin {
+	/**
+	 * Экземпляр для прослушивания событий пользователя
+	 */
+    private final PlayerListener playerListener = new PlayerListener(this);
+
+	/**
+	 * Объект для логирования сообщений плагина
+	 */
+	Logger log = Logger.getLogger("Minecraft");
+	
+	/**
+	 * Активация плагина
+	 */
+    @Override
+	public void onEnable(){
+		// Активирую прослушивание событий пользователя
+		PluginManager pm = getServer().getPluginManager();
+        pm.registerEvents(playerListener, this);
+        
+        userList();
+
+		log.info("MyIRC загружен!");
+	}
+ 
+	/**
+	 * Деактивация плагина
+	 */
+    @Override
+	public void onDisable(){
+        log.info("MyIRC отключен.");
+	}
+    
+    /**
+     * Печатает список пользователей в консоль
+     */
+    private void userList() {
+    	for (Player player : getOnlinePlayers()) {
+            Location playerLocation = player.getLocation();
+            log.info(player.getName() + ": " + playerLocation.getBlockX() + "/" + playerLocation.getBlockZ());
+        }
+    }
+    
+    /**
+     * @return Player[] Возвращает список игроков
+     */
+    public static List<Player> getOnlinePlayers() {
+        List<Player> list = Lists.newArrayList();
+        for (World world : Bukkit.getWorlds()) {
+            list.addAll(world.getPlayers());
+        }
+        return Collections.unmodifiableList(list);
+    }
+}
---
+++ src/me/truekenny/MyIRC/PlayerListener.java
@@ -0,0 +1,56 @@
+package me.truekenny.MyIRC;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerChatEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+
+/**
+ * Класс-слушатель событий происходящих с пользователем
+ * @author truekenny
+ */
+@SuppressWarnings("deprecation")
+public class PlayerListener implements Listener {
+	/**
+	 * Экземпляр главного класса плагина
+	 */
+    private final MyIRC plugin;
+
+	/**
+     * Сохранение объекта главного класса
+     * @param instance
+     */
+    public PlayerListener(MyIRC instance) {
+        plugin = instance;
+        plugin.getLogger().info("PlayerListener загружен.");
+    }
+
+    /**
+     * Обрабытывает вход пользователя
+     * @param event
+     */
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        plugin.getLogger().info(event.getPlayer().getName() + " вошел в игру.");
+    }
+
+    /**
+     * Обрабатывает выход пользователя
+     * @param event
+     */
+    @EventHandler
+    public void onPlayerQuit(PlayerQuitEvent event) {
+        plugin.getLogger().info(event.getPlayer().getName() + " вышел из игры.");
+    }
+
+    /**
+     * Обрабатывает сообщения пользователя 
+     * @param event
+     */
+    @EventHandler
+    public void onPlayerChat(PlayerChatEvent event)
+    {
+      plugin.getLogger().info(event.getPlayer().getName() + " написал «" + event.getMessage() + "»");
+    }
+}
